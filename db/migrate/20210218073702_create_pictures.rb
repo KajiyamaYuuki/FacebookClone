4234e8218fc5510ed60eabc791deab3f4d900096
yuuki kajiyama <luv4u.1331@gmail.com>
2021-02-18 17:04:26 +0900

    feat.:モデルの作成

---
+++ db/migrate/20210218073702_create_pictures.rb
@@ -0,0 +1,9 @@
+class CreatePictures < ActiveRecord::Migration[5.2]
+  def change
+    create_table :pictures do |t|
+      t.text :image
+      t.references :post, foreign_key: true
+      t.timestamps
+    end
+  end
+end
