a85c6bbe7abf2c8b2622de4dd8f7722422de1eed
yuuki kajiyama <luv4u.1331@gmail.com>
2021-02-20 02:09:58 +0900

    feat.:ログインシステムの追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,19 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to user_path(@user.id)
+    else
+      render.new
+    end
+  end
+  def show
+    @user = User.find(prams[:id])
+  end
+  private
+  def user_params
+    prams.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 end
