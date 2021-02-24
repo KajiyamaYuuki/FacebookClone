849a38bface20a1bb6ccf7a924552b124d39e54c
yuuki kajiyama <luv4u.1331@gmail.com>
2021-02-24 23:58:23 +0900

    feat.:外部キー周辺の設定

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,9 +3,11 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+
   def new
     @post = Post.new
   end
+
   def create
     @post = current_user.posts.build(post_params)
     if params[:back]
@@ -18,10 +20,13 @@ class PostsController < ApplicationController
       end
     end
   end
+
   def show
   end
+
   def edit
   end
+
   def update
     if @post.update(post_params)
       redirect_to posts_path, notice: "投稿を編集しました"
@@ -29,6 +34,7 @@ class PostsController < ApplicationController
       render :edit
     end
   end
+
   def destroy
     @post.destroy
     redirect_to posts_path, notice:"投稿を削除しました"
@@ -37,10 +43,12 @@ class PostsController < ApplicationController
     @post = current_user.posts.build(post_params)
     render :new if @post.invalid?
   end
+
   private
   def post_params
     params.require(:post).permit(:content, :image, :image_cache)
   end
+
   def set_post
     @post = Post.find(params[:id])
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
   def create
     @user = User.new(user_params)
     if @user.save
@@ -10,9 +11,12 @@ class UsersController < ApplicationController
       render :new
     end
   end
+
   def show
     @user = User.find(params[:id])
+    @posts = @user.posts
   end
+
   private
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
